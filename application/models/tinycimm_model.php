2d811ee0dddbf5a8290ebfa618a40ab503adbde7
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-03-19 23:09:59 +0000

    comments mainly

--- application/models/tinycimm_model.php
+++ application/models/tinycimm_model.php
@@ -12,7 +12,7 @@ class Tinycimm_model extends Model {
 	/**
 	* get an asset from the database
 	*
-	* @param integer|$image_id The id of the image to retrieve
+	* @param integer|$asset_id The id of the image to retrieve
 	* @return Object| an object containing full database row for the image
 	**/
 	function get_asset($asset_id){
@@ -22,7 +22,7 @@ class Tinycimm_model extends Model {
 	/**
 	* Deletes an asset's data from the database
 	*
-	* @param integer|$image_id The id of the image to delete
+	* @param integer|$asset_id The id of the image to delete
 	**/
 	function delete_asset($asset_id){
 		$this->db->where('id', (int)$asset_id)->delete('asset');	
