266be88051ed2cb7dc80a6abc5c33c3694770f07
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-04-21 20:22:30 +0000

    updated views

--- trunk/application/views/tinycimm/image/image_folder_list.php
+++ trunk/application/views/tinycimm/image/image_folder_list.php
@@ -1,12 +1,21 @@
 <ul class="folderlist">
-	<?foreach($folders AS $folder):?>
+	<?foreach($folders as $folder):?>
 	<li>
 		<span style="display:block" id="folder-<?=$folder['id'];?>" onMouseOver="this.style.color='#000066';this.style.background='#EEEEEE';" onMouseOut="this.style.color='#000000';this.style.background='#FFFFFF';">
 			<span class="editimg">
-				<a href="javascript:;"><img height="13" onclick="editFolder('53');" title="edit" src="img/pencil_sm.png"/></a>
-				<a href="javascript:;"><img onclick="ImageDialog.deleteFolder('<?=$folder['id'];?>');" title="remove" src="img/delete.gif"/></a>
+				<!--
+				<a href="javascript:;">
+					<img height="13" onclick="editFolder('53');" title="edit" src="img/pencil_sm.png"/>
+				</a>
+				-->
+				<a href="javascript:;">
+					<img onclick="ImageDialog.deleteFolder('<?=$folder['id'];?>');" title="remove" src="img/delete.gif"/>
+				</a>
 			</span>
-			<span style="cursor:pointer;" onClick="ImageDialog.fileBrowser('<?=$folder['id'];?>');"><img class="folderimg" id="img-<?=$folder['id'];?>" src="img/folder.gif" /> <?=$folder['name'];?>/</span>
+			<span style="cursor:pointer;" onClick="ImageDialog.fileBrowser('<?=$folder['id'];?>');">
+				<img class="folderimg" id="img-<?=$folder['id'];?>" src="img/folder.gif" />
+				<?=$folder['name'];?>/
+			</span>
 		</span>
 		<br class="clear" />
 	</li>
