4bafd850b0a6bab2a0e47b0cbea409234218b159
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-04-21 18:31:43 +0000

    comments updated

--- trunk/application/models/tinycimm_model.php
+++ trunk/application/models/tinycimm_model.php
@@ -15,12 +15,19 @@ class Tinycimm_model extends Model {
 	* @param integer|$asset_id The id of the image to retrieve
 	* @return Object| an object containing full database row for the image
 	**/
-	function get_asset($asset_id){
+	function get_asset($asset_id=0){
 		return $this->db->where('id', (int) $asset_id)->get('asset', 1)->row();
 	}
 
-	function get_assets($folder_id=""){
-		return $this->db->where('folder_id', $folder_id)->order_by('dateadded', 'desc')->get('asset')->result_array();
+	
+	/**
+	* Get a list of assets by folder from the database
+	*
+	* @param integer|$folder_id The id of the folder the assets are related to
+	* @return Object| a result object containing rows for the assets
+	**/
+	function get_assets($folder_id=0){
+		return $this->db->where('folder_id', (int) $folder_id)->order_by('dateadded', 'desc')->get('asset')->result_array();
 	}
 	
 	/**
@@ -50,6 +57,11 @@ class Tinycimm_model extends Model {
 		return $this->db->insert_id();
 	}
 
+	/** 
+	* Inserts a folder into the database
+	*
+	* @returns integer|insert_id the last insert id from the id sequence colmn
+	**/
 	function insert_folder($folder_name=''){
 		$fields = array('name' => $folder_name);
 		$this->db->set($fields)->insert('asset_folder');
@@ -57,16 +69,17 @@ class Tinycimm_model extends Model {
 	}
 	
 	/**
-	* Get all image folders
+	* Get all image folders, or folders owned by $user_id
 	*
 	* @param String|$orderby the method to sort the results by
-	* @return Array| the full query array
+	* @param Integer|$user_id 
+	* @return Object| a result object of the list of folder from the database
 	**/
-	function get_folders($orderby='name', $user_id=FALSE){
+	function get_folders($order_by='name', $user_id=FALSE){
 		if ($user_id === FALSE) {
-			return $this->db->order_by($orderby, 'asc')->get('asset_folder')->result_array();
+			return $this->db->order_by($order_by, 'asc')->get('asset_folder')->result_array();
 		} else {
-			return $this->db->where('user_id', (int) $user_id)->order_by($orderby, 'asc')->get('asset_folder')->result_array();
+			return $this->db->where('user_id', (int) $user_id)->order_by($order_by, 'asc')->get('asset_folder')->result_array();
 		}
 	}
 
