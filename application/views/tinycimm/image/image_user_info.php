eb28ffa13dffe5a4548fa67a55f5d45b3486c119
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-05-05 18:47:21 +0000

    removed legacy views

--- application/views/tinycimm/image/image_user_info.php
+++
@@ -1,6 +0,0 @@
-<table border="0" cellpadding="4" cellspacing="0">
-	<tr><td>Images uploaded:</td><td><?=$user['tot_images'];?> <!-- (<?=$user['tot_gif'];?> gif, <?=$user['tot_jpg'];?> jpg, <?=$user['tot_png'];?> png)--></td></tr>
-	<tr><td>Space used:</td><td>1.8mb</td></tr>
-	<tr><td>Space remaining:</td><td>3.2mb</td></tr>
-	<tr><td>Allowed Types:</td><td>jpg, gif, png</td></tr>
-</table>
