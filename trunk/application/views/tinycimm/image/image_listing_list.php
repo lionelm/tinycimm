332b11f712cfec74d73a41130d38b5d0fb19f691
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-05-15 19:19:15 +0000

    browser fixes

--- trunk/application/views/tinycimm/image/image_listing_list.php
+++ trunk/application/views/tinycimm/image/image_listing_list.php
@@ -36,7 +36,12 @@
 		</td>
 		<td width="5">&nbsp;</td>
 		<td valign="top">
-			<div class="heading">&raquo; <?=$selected_folder_info['name'];?></div>
+			<div class="heading">
+				<input type="text" id="search-input" onblur="this.value=this.value==''?'search..':this.value;" onfocus="this.value=this.value=='search..'?'':this.value;" value="search.." />
+				<img src="img/ajax-loader-sm.gif" id="search-loading" style="margin:2px 2px 0px 0px;" class="right hidden" />
+				&raquo; 
+				<?=$selected_folder_info['name'];?>
+			</div>
 			<div id="filelist">
 				<ul class="folderlist">
 				<?if (sizeof($images) == 0) {?>
