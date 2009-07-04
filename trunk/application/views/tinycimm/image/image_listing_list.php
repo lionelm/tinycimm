c62e81855f883c8fe246e36c6272f1873928ac3d
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-07-04 21:43:30 +0000

    splitting out html into subtpl's

--- trunk/application/views/tinycimm/image/image_listing_list.php
+++ trunk/application/views/tinycimm/image/image_listing_list.php
@@ -1,46 +1,12 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
 	<tr>
 		<td width="178" valign="top">
-			<div id="addfolder" style="display:none;">
-				<div class="heading">&raquo; Add Folder</div>
-				<input type="text" id="add_folder_caption" class="input" style="width:156px;float:left;margin-right:5px">
-				<img src="img/save.gif" onClick="TinyCIMMImage.addFolder();" style="cursor:pointer;float:left" alt="save folder" title="save folder" />
-				<div style="clear:left"></div>
-			</div>
-			<div class="heading">
-				<span style="float:right;padding-right:2px;font-weight:normal">
-					[<a href="#" onclick="tinyMCEPopup.dom.get('addfolder').style.display='block';tinyMCEPopup.dom.get('add_folder_caption').focus()">add</a>]
-				</span>&raquo; Folders
-			</div>
-			<div id="folderlist">
-				<?= $this->load->view($this->view_path.'image_folder_list');?>
-			</div>
-			<br/>
-			<div class="heading">&raquo; Folder Info</div>
-			<table border="0" cellpadding="2" cellspacing="1">
-				<tr><td>Images:</td><td><?=$selected_folder_info['total_assets'];?></td></tr>
-				<tr><td>Size:</td><td><?=$selected_folder_info['total_file_size'];?></td></tr>
-				<!--<tr><td>Owner:</td><td><?=$selected_folder_info['username'];?></td></tr>-->
-				<tr>
-					<td>View:</td>
-					<td>
-						<select style="border:1px solid #AAA" onchange="TinyCIMMImage.changeView(this.options[this.selectedIndex].value)">
-							<optgroup label="Views">
-								<option value="listing" selected>File Listing</option>
-								<option value="thumbnails">Thumbnails</option>
-							</optgroup>
-						</select>
-					</td>
-				</tr>
-			</table>
+			<?= $this->load->view($this->view_path.'subtpl/leftpane');?>
 		</td>
 		<td width="5">&nbsp;</td>
 		<td valign="top">
 			<div class="heading">
-				<input type="text" id="search-input" onblur="this.value=this.value==''?'search..':this.value;" onfocus="this.value=this.value=='search..'?'':this.value;" value="search.." />
-				<img src="img/ajax-loader-sm.gif" id="search-loading" style="margin:2px 2px 0px 0px;" class="right hidden" />
-				&raquo; 
-				<?=$selected_folder_info['name'];?>
+				<?= $this->load->view($this->view_path.'subtpl/search');?>
 			</div>
 			<div id="filelist">
 				<ul class="folderlist">
