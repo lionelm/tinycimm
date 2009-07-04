36bf4e3dcfe38e7ba5afe840ee088c8a2e8001cb
willis.rh <willis.rh@b4a5b0b9-3044-0410-b134-89c37e818cb4>
2009-07-04 23:42:08 +0000

    clicking on 'All images' now actually displays all images. I also removed the 'General' folder from the upload folder dropdown..

--- trunk/application/views/tinycimm/image/image_folder_select.php
+++ trunk/application/views/tinycimm/image/image_folder_select.php
@@ -1,6 +1,6 @@
 <select name="uploadfolder" id="folderselect">
 	<optgroup label="Folders">
-		<option value="">General</option>
+		<option value="">(none)</option>
 		<?foreach($folders AS $folderinfo):?>
 		<option value="<?=$folderinfo['id'];?>"<?=($folder_id==$folderinfo['id'])?' selected':'';?>><?=$folderinfo['name'];?></option>
 		<?endforeach;?>
